1ca031ebf4b37039fcf1f1030d5b03c0f7a42a36
Muskan <muskankhushi2022@gmail.com>
2025-05-26 09:46:27 +0530

    .

---
+++ 3-styling/src/App.jsx
@@ -0,0 +1,17 @@
+//HOW TO STYLE REACT COMPONENTS WITH CSS
+
+//not using external frameworks or preprocessors
+//1.EXTERNAL
+//2. MODULE
+//3. INLINE
+
+
+function App() {
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
