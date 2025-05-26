4e738bf2eb3a7b29512e1ec7b3e61121ca05b46d
Muskan <muskankhushi2022@gmail.com>
2025-05-26 10:00:11 +0530

    /

--- 3-styling/src/Button/Button.jsx
+++ 3-styling/src/Button/Button.jsx
@@ -1,9 +1,23 @@
 import styles from './Button.module.css'
 
+// const styles = {
+//   backgroundColor: "hsl(200, 100%, 50%)",
+//   color: "white",
+//   padding: "10px 20px",
+//   borderRadius: "5px",
+//   border: "none",
+//   cursor: "pointer",
+
+// }
+
 function Button(){
   return(
     <>
+    {/* /*modules css*/ }
      <button className={styles.button}>Click me</button>
+
+      {/* inline css
+     <button style={styles}></button> */}
     </>
   );
 }
